cadc1fd26fc724673efb64b771acfca606d17965
Gwathlo <gwath@yahoo.com>
2022-04-14 11:28:46 +0200

    Add Angular Flex-layout

--- src/app/heroes/heroes.module.ts
+++ src/app/heroes/heroes.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { HeroesRoutingModule } from './heroes-routing.module';
+
 import { InsertComponent } from './pages/insert/insert.component';
 import { SearchComponent } from './pages/search/search.component';
 import { HeroComponent } from './pages/hero/hero.component';
 import { HomeComponent } from './pages/home/home.component';
 import { ListComponent } from './pages/list/list.component';
-import { HeroesRoutingModule } from './heroes-routing.module';
 
 @NgModule({
   declarations: [
@@ -15,6 +17,6 @@ import { HeroesRoutingModule } from './heroes-routing.module';
     HomeComponent,
     ListComponent,
   ],
-  imports: [CommonModule, HeroesRoutingModule],
+  imports: [CommonModule, FlexLayoutModule, HeroesRoutingModule],
 })
 export class HeroesModule {}
